102f44780e5cb4580f0f54517a56833c6d8785c2
‘Elena <tcachelena@gmail.com>
2025-02-10 12:59:07 +0500

    afd

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,38 +1,38 @@
 export const sendMail = () => {
-  document
-    .querySelector(".js-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+  const formPage = document.querySelector(".js-form");
 
-      const formData = new FormData();
-      //   formData.append("name", name);
-      //   formData.append("phone", phone);
-      //   formData.append("message", message);
+  formPage.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      fetch("/inc/submit-form.php", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.success) {
-            // onShowMenu(popup);
-            console.log("ok", data.message);
-            document.getElementById("response-message").innerHTML =
-              `<p style="color: green;">${data.message}</p>`;
-            document.querySelector(".js-form").reset();
-          } else {
-            console.log("Else", data.message);
-            document.getElementById("response-message").innerHTML =
-              `<p style="color: red;">${data.message}</p>`;
-          }
-          document.getElementById("response-message").style.display = "block";
-        })
-        .catch((error) => {
-          console.log("error", error);
+    const formData = new FormData(formPage);
+    //   formData.append("name", name);
+    //   formData.append("phone", phone);
+    //   formData.append("message", message);
+
+    fetch("/inc/submit-form.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // onShowMenu(popup);
+          console.log("ok", data.message);
           document.getElementById("response-message").innerHTML =
-            `<p style="color: red;">Произошла ошибка. Пожалуйста, попробуйте позже.</p>`;
-          document.getElementById("response-message").style.display = "block";
-        });
-    });
+            `<p style="color: green;">${data.message}</p>`;
+          document.querySelector(".js-form").reset();
+        } else {
+          console.log("Else", data.message);
+          document.getElementById("response-message").innerHTML =
+            `<p style="color: red;">${data.message}</p>`;
+        }
+        document.getElementById("response-message").style.display = "block";
+      })
+      .catch((error) => {
+        console.log("error", error);
+        document.getElementById("response-message").innerHTML =
+          `<p style="color: red;">Произошла ошибка. Пожалуйста, попробуйте позже.</p>`;
+        document.getElementById("response-message").style.display = "block";
+      });
+  });
 };
